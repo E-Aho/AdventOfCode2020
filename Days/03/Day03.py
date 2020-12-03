6264117574a6cbffd6630de3a276ae0ddfa90e7f
Erik Aho <erik.aho@argos.co.uk>
2020-12-03 05:25:46 +0000

    Part 3

--- Days/03/Day03.py
+++ Days/03/Day03.py
@@ -3,27 +3,27 @@ def get_input(file_location: str):
     arr = []
     with open(file_location, "r") as file:
         for line in file.readlines():
-            arr.append(line)
-        #     split_line = line.split(" ")
-        #     int_0 = int(split_line[0].split("-")[0])
-        #     int_1 = int(split_line[0].split("-")[1])
-        #     letter = split_line[1][:-1]
-        #     password = split_line[2].strip()
-        #     arr.append(
-        #         {"int_0": int_0,
-        #          "int_1": int_1,
-        #          "letter": letter,
-        #          "pw": password}
-        #     )
+            arr.append(list(line.strip()))
+    print(arr[0])
     return arr
 
 
-def part_1(parsed_input: list):
-    return None
+def part_1(parsed_input: list, x: int = 1, y:int = 3):
+    count = 0
+    for i in range(int(len(parsed_input)/x)):
+        if parsed_input[x*i][(y*i % len(parsed_input[i]))] == "#":
+            count += 1
+    return count
 
 
 def part_2(parsed_input: list):
-    return None
+    a = part_1(parsed_input, x=1, y=1)
+    b = part_1(parsed_input, x=1, y=3)
+    c = part_1(parsed_input, x=1, y=5)
+    d = part_1(parsed_input, x=1, y=7)
+    e = part_1(parsed_input, x=2, y=1)
+
+    return a * b * c * d * e
 
 
 if __name__ == "__main__":
